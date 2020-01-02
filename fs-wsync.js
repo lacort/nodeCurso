f601caa269c01a5a4fd604bf4d635c8015462036
lacort <lacort2@gmail.com>
2020-01-02 13:49:48 -0300

    Bloking no bloking

---
+++ fs-wsync.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+
+console.log((process.hrtime()[0]/60).toFixed(5));
+console.log("ANTES DA LEITURA");
+
+const dados = fs.readFile("arquivo.txt", (err,data)=> {
+    if (err) throw err;
+
+});
+
+
+
+console.log((process.hrtime()[0]/60).toFixed(5));
+console.log("DEPOIS DA LEITURA");
