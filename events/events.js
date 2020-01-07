bc42a71dca7b44da8427ecee94b86922e4b1c8dc
lacort <lacort2@gmail.com>
2020-01-07 09:57:08 -0300

    Events

---
+++ events/events.js
@@ -0,0 +1,12 @@
+const EventEmitter = require('events');
+
+class Evento extends EventEmitter {};
+
+const myEvent = new Evento();
+
+
+myEvent.on('security', (x,y)=>{
+    console.log(`Executando o evento 'Segurança': ${x} ${y}`)
+});
+
+myEvent.emit('security', 'userAdmin', 'Alterou Salário');
