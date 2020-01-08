3a7dafc0edacbc6c54c58db993d7985e9f0bb6ef
lacort <lacort2@gmail.com>
2020-01-08 18:34:06 -0300

    Api HTTP

---
+++ http/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+
+const port = 3000;
+
+const url = `http://${hostname}:${port}/`;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Bem vindo<br/>Node.js</h1>');
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Servidor Rodando em ${url}`)
+})
+
+const open = (process.platform == 'darvin' ? 'open' : process.platform == 'win32' ? 'start' : 'xdg-open');
+
+require('child_process').exec(open + ' ' + url);
+
